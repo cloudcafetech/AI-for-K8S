194677d6f09bf797d65b97cb7e448bb7b00566e1
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2023-12-01 12:34:51 +0530

    Update k8s-setup.sh

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -145,11 +145,12 @@ kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 
 # Install krew
 set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
-  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
-  "$KREW" update
+OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+KREW="krew-${OS}_${ARCH}" &&
+curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+tar zxvf "${KREW}.tar.gz" &&
+./"${KREW}" install krew
   
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
